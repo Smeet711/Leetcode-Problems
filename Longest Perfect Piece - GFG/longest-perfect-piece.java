4bb9a6068e313903e83c60ffa87bdfb1a2862f95
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2022-11-18 11:16:15 +0530

    Added solution - LeetHub

--- Longest Perfect Piece - GFG/longest-perfect-piece.java
+++ Longest Perfect Piece - GFG/longest-perfect-piece.java
@@ -32,20 +32,23 @@ class Solution {
     static int longestPerfectPiece(int[] arr, int N) {
         // code here
         
-        int i=0;
-        int j=0;
-        int ans=0;
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        int ans = 0;
         
-        while(i < N){
-            if(Math.abs(arr[i] - arr[j] ) <= 1){
-                ans = Math.max(ans,i-j+1);
-                i++;
-            }else{
-                if(Math.abs(arr[i] - arr[j]) > 1){
-                    j++;
-                }
-                
+        for(int i=0,j=0;i<N;i++){
+            
+            min.add(arr[i]);
+            max.add(arr[i]);
+            
+            while(j < N && max.peek() - min.peek() > 1){
+                min.remove(arr[j]);
+                max.remove(arr[j]);
+                j++;
             }
+            
+            ans = Math.max(ans,i-j+1);
+            
         }
         return ans;
         
