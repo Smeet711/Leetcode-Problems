c65d60655f051792de424c8756cd4b6adb104ac4
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2022-11-20 15:56:07 +0530

    Time: 3 ms (88.67%), Space: 44.6 MB (66.79%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0,candidates,target,ans,new ArrayList<>());
+        
+        return ans;
+        
+    }
+    
+    //
+    public void findCombinations(int index,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        
+        if(index == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        // pick 
+        if(arr[index] <= target){
+            ds.add(arr[index]);
+            findCombinations(index,arr,target-arr[index],ans,ds);
+            ds.remove(ds.size() - 1 );
+        }
+        
+        // not pick
+        findCombinations(index+1,arr,target,ans,ds);
+        
+    }
+    
+}
