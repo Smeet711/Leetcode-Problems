1c372e4095f5ff7a663b026dbb219ac136410d7a
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2022-11-22 12:16:06 +0530

    Time: 2 ms (65.47%), Space: 41.8 MB (41.51%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+        Integer[] arr = new Integer[rowIndex+1];
+        Arrays.fill(arr,0);
+        
+        arr[0] = 1;
+        
+        for(int i=0;i<=rowIndex;i++){
+            for(int j=i;j>0;j--){
+                arr[j] = arr[j] + arr[j-1];
+            }
+        }
+        
+        return Arrays.asList(arr);
+    }
+}
