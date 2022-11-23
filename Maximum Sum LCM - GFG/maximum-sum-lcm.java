e785f900b4be04bf76b095efa8be610257ba65d7
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2022-11-23 09:54:06 +0530

    Added solution - LeetHub

--- Maximum Sum LCM - GFG/maximum-sum-lcm.java
+++ Maximum Sum LCM - GFG/maximum-sum-lcm.java
@@ -25,6 +25,7 @@ class Solution {
         
         long sum = 0;
         int num = (int)Math.sqrt(n);
+        
         for(int i=1;i<=num;i++){
             if(n%i == 0){
                 sum += i;
@@ -33,6 +34,10 @@ class Solution {
                 }
             }
         }
+        
+        
+        
         return sum;
+       
     }
 }
